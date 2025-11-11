7357ca11518c3c4dd2bf0a96807d54c4bd8576ea
Michael Appleby <michael.appleby@yale.edu>
2025-11-11 09:11:25 -0500

    Move transcribe method

--- iiif_ai_helpers/utils.py
+++ iiif_ai_helpers/utils.py
@@ -1,3 +1,7 @@
+import anthropic
+import base64
+import httpx
+import uuid
 
 # retrieves the first image service of the first painting annotation
 def get_image_service(canvas):
@@ -16,3 +20,44 @@ def get_image_service(canvas):
       service = first_painting_anno_body['service']
     # Handle Image API v3 or v2
     return service.get('@id')
+
+# Create an image URL, scaled to a size appropriate for the AI service used
+# Claude requests max of 1568x1568
+def get_image(image_service):
+    image_url = f"{image_service}/full/!1568,1568/0/default.jpg"
+    media_type = "image/jpeg"
+    # encode data
+    image_data = base64.standard_b64encode(httpx.get(image_url).content).decode("utf-8")
+    return {
+        "image_data": image_data,
+        "media_type": media_type,
+        "image_url": image_url
+    }
+
+# Call the Anthropic API
+def transcribe_image(image_data, media_type, key):
+    client = anthropic.Anthropic(api_key=key)
+    message = client.messages.create(
+        model="claude-sonnet-4-20250514",
+        max_tokens=4000,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image",
+                        "source": {
+                            "type": "base64",
+                            "media_type": media_type,
+                            "data": image_data,
+                        },
+                    },
+                    {
+                        "type": "text",
+                        "text": "Transcribe the text of the document in this image.  Do not describe the document or provide commentary, just the transcription."
+                    }
+                ],
+            }
+        ],
+    )
+    return message.content[0].text
